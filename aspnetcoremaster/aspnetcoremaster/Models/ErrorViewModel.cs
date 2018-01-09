b1f62a056efb6edbc0f84f4abd62828ef3895a46
Asif Rahman <asif0531@live.com>
2018-01-09 20:01:18 +0600

    initial commit

---
+++ aspnetcoremaster/aspnetcoremaster/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace aspnetcoremaster.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
