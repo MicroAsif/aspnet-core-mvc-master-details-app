422218d96dbc145829c3813d7d28fcf6b8ca9713
Asif Rahman <asif0531@live.com>
2018-01-11 23:23:15 +0600

    DbConetxt added, need to do migraions

---
+++ aspnetcoremaster/aspnetcoremaster.data/AppDbContext/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using aspnetcoremaster.core.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace aspnetcoremaster.data.AppDbContext
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<CustomerModel> Customer { get; set; }
+        public DbSet<ProductModel> Product { get; set; }
+
+    }
+}
